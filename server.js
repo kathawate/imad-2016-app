df6c68eacaf26666f153e6914196219c5b8777a2
kathawate <srkathawate@gmail.com>
2016-09-20 23:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function (req,res){
 });
 
 app.get('/article-two',function (req,res){
-    res.send('article-second requested and will be served here');
+    res.send('article-second requested and will be served here hi');
 });
 
 app.get('/article-three',function (req,res){
