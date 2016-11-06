4ad5ca44944016cafe78411e54fa0c875482774e
kathawate <srkathawate@gmail.com>
2016-11-06 08:16:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.get('/article-two',function (req,res){
     res.send('article-second requested and will be served here');
 });
 
-app.get('/article-three',function (req,res){
-    res.send('article-third requested and will be served here');
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
