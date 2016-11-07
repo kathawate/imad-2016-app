4a3515d80f6a81e4bc4596d7d62a883f2da1f314
kathawate <srkathawate@gmail.com>
2016-11-08 01:16:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,7 @@ button.onclick=function(){
 
 
 
- var nameInput=document.getElementById('name');
- var name =nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
      var request= new XMLHttpRequest();
@@ -43,7 +42,8 @@ submit.onclick=function(){
       }
       
   };
- 
+  var nameInput=document.getElementById('name');
+ var name =nameInput.value;
   request.open('GET','http://kathawate.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
   
