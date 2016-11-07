b17c45b8805fa69261039ba6ff402bdbbbbe0629
kathawate <srkathawate@gmail.com>
2016-11-07 12:17:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element= document.getElementById('main-text');
 
 element.innerHTML ='shubham keep going on.....';
 
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 
 img.onclick=function(){
     
