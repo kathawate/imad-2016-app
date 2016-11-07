42459236f09cc7578d069d2b3e306ae9aef23353
kathawate <srkathawate@gmail.com>
2016-11-07 09:14:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,10 +6,57 @@ var app = express();
 app.use(morgan('combined'));
 
 
+var articleone={
+  title:'article-one |kathawate',
+  heading:'article-one',
+  date:'5 sept 2016',
+  content: `<p>
+         My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+          My name is shubham kathawate.i'm 21 years old and i love the programming...
+        </p>
+        <p>
+             My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+            </p>`
+    
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=`
+    <!doctype html>
+ <head>
+     <title>
+          ${title}
+     </title>     
+     <meta name="viewport" content="width=device-width ,initial-scale=1"/>
+       <link href="/ui/style.css" rel="stylesheet" />
+    
+ </head>
+ <body>
+     <div class ="container">
+     <div>
+         <a href="/">Home</a>
+     </div>  
+     <hr/>
+     <h1>
+         ${heading}
+        </h1>
+    <div>
+        ${date}
+    </div>
+    <div>
+       ${content}
+    </div>
+    </div>
+ </body>     
 
+</html>
+`;
 
-
-
+return htmlTemplate;
+}
 
 
 
@@ -18,7 +65,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
