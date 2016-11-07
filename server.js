2bc63dc7178dd7df2b45c59d460f9d465a190e2e
kathawate <srkathawate@gmail.com>
2016-11-07 19:24:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/ui/main.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     var name= req.query.name;
     var name;
     names.push(name);
