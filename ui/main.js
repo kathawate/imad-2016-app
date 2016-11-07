c095d59c39f6fdbdcfda3d9d1824583a18398bf4
kathawate <srkathawate@gmail.com>
2016-11-08 01:27:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,10 +32,10 @@ submit.onclick=function(){
             if (request.status ===200)  {
              var names=request.responseText;
              names=JSON.parse(names);
-    var list='';
-    for(var i=0 ;i<names.lenght ; i++){
-        list +='<li>' +names[i] + '</li>';
-    }
+             var list='';
+             for(var i=0 ;i<names.lenght ; i++){
+                list +='<li>' +names[i] + '</li>';
+                }
     var ul =document.getElementById('namelist');
     ul.innerHTML =list;
             }
