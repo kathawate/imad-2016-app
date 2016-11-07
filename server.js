19060e7ec20050582d750ff3b11a8e1089de5a01
kathawate <srkathawate@gmail.com>
2016-11-07 08:48:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,6 +61,11 @@ var htmlTemplate=`
     return htmlTemplate;
 }
 
+var articleName=req.params.articleName;
+
+app.get('/articleone',function (req,res){
+    res.send(createTemplate(articleone));
+});
 
 
 
@@ -84,9 +89,9 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
+//app.get('/article-one', function (req, res) {
+ // res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+//});
 
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
