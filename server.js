5519ce5b9a76433093785c812d85f2e9a0ab0f5d
kathawate <srkathawate@gmail.com>
2016-11-07 09:23:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,8 +88,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
-  res.send(createTemplate(articles[articleName]));
+app.get('/article-one', function (req, res) {
+  res.send(createTemplate(articleone));
 });
 var articleName=req.params.articleName;
 app.get('/article-two', function (req, res) {
