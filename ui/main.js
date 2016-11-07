cb07f4b08b11df887ca3cc4f81576f3050dcf204
kathawate <srkathawate@gmail.com>
2016-11-07 19:49:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,9 @@ button.onclick=function(){
   request.onreadystatechange =function(){
       if(request.readystate === XMLHttpRequest.DONE){
             if (request.status ===200)  {
-               
+             var counter=request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
             }
       }
       
@@ -19,6 +21,26 @@ var nameInput=document.getElementById('name');
 var name =nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
+     var request= new XMLHttpRequest();
+  
+  request.onreadystatechange =function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+            if (request.status ===200)  {
+             var names=request.responseText;
+             names=JSON.parse(name);
+    var list='';
+    for(var i=0 ;i<names.lenght:i++){
+        list +='<li>' +names[i] +</li>
+    }
+    var ul =document.getElementById('namelist');
+    ul.innerHTML =list;
+            }
+      }
+      
+  };
+  
+  request.open('GET','http://kathawate.imad.hasura-app.io/submit-name?name='+name,true);
+  request.send(null);
     var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0 ;i<names.lenght:i++){
