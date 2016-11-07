4d5221948ec5f96003ec576a77cc02afd283fd44
kathawate <srkathawate@gmail.com>
2016-11-07 12:26:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,12 @@ var element= document.getElementById('main-text');
 element.innerHTML ='shubham keep going on.....';
 
 var img=document.getElementById('madi');
-
+var marginLeft =0;
+function moveRight(){
+    marginLeft=marginLeft +10;
+    img.style.marginLeft =marginLeft + 'px';
+}
 img.onclick=function(){
-    
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+   // img.style.marginLeft='100px';
 };
