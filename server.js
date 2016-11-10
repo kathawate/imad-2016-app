bc1136d0a0d040c048758269553d06f538c1db85
kathawate <srkathawate@gmail.com>
2016-11-10 09:35:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,14 +131,31 @@ app.get('/', function (req, res) {
 
 
 function hash(input ,salt){
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
     var hashedString =hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
     
 });
+
+app.get('/create-user',function(req,res){
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString =hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+         if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send('User sucessfully created :'+username);
+       } 
+        
+    });
+});
+
+
+
+
 
 
 var pool = new Pool(config);
