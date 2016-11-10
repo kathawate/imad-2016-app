a1c7ceff4c1674ae2bda92d45b38f9fa27edff70
kathawate <srkathawate@gmail.com>
2016-11-10 09:05:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
-
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
+
+
+
 var config = {
   user :'kathawate',
   database:'kathawate',
@@ -127,6 +130,15 @@ app.get('/', function (req, res) {
 });
 
 
+function hash(input ,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString =hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+    
+});
 
 
 var pool = new Pool(config);
