899fbed963ad8156eeee3f0e6076ad960fb9e529
kathawate <srkathawate@gmail.com>
2016-11-17 23:50:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,25 +2,175 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+
+var pool=require('pg').pool;
+var config = {
+  user :'kathawate',
+  database:'kathawate',
+  host:'db.imad.hasura-app.io',
+  port:'5432',
+  password: process.env.DB_PASSWORD,
+};
+
+
+
 var app = express();
 app.use(morgan('combined'));
+ var articleone={
+    title:' article-one |kathawate',
+    heading:'article-one',
+    date:'nov 6, 2016',
+    content: `<p>
+         My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+          My name is shubham kathawate.i'm 21 years old and i love the programming...
+        </p>
+        <p>
+             My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+            </p>`
+};
+var articletwo={
+     title:' article-two |kathawate',
+    heading:'article-two',
+    date:'nov 6, 2016',
+    content: `<p>
+         My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+          My name is shubham kathawate.i'm 21 years old and i love the programming...
+        </p>
+        <p>
+             My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+            </p>`
+    
+};
+  var articlethree={
+     title:' article-three |kathawate',
+    heading:'article-three',
+    date:'nov 6, 2016',
+    content: `<p>
+         My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+          My name is shubham kathawate.i'm 21 years old and i love the programming...
+        </p>
+        <p>
+             My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
+            </p>`
+};
+function createTemplate(data){
+    var title=data.title;
+     var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+   
+
+var htmlTemplate=`
+<html>
+ <head>
+     <title>
+         ${title}
+     </title>     
+     <meta name="viewport" content="width=device-width ,initial-scale=1"/>
+       <link href="/ui/style.css" rel="stylesheet" />
+    
+ </head>
+ <body>
+     <div class ="container">
+     <div>
+         <a href="/">Home</a>
+     </div>  
+     <hr/>
+     <h1>
+         ${heading}
+        </h1>
+    <div>
+        ${date}
+    </div>
+    <div>
+         ${content}
+    </div>
+    </div>
+ </body>     
+
+</html>
+
+`;
+    return htmlTemplate;
+}
+
+
+
+//app.get('/:articleName',function (req,res){
+//var articleName=req.params.articleName;
+ //   res.send(createTemplate(articles[articleName]));
+//});
+
+app.get('/articleone', function (req, res) {
+   res.send(createTemplate(articleone));
+});
+
+app.get('/articletwo', function (req, res) {
+   res.send(createTemplate(articletwo));
+});
+app.get('/articlethree', function (req, res) {
+   res.send(createTemplate(articlethree));
+});
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/shubham', function (req, res) {
-  res.send(path.join('hiiii'));
+
+
+
+var pool = new pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM article',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result.rows));
+       }
+    });
 });
+
+
+
+
+
+
+
+var counter=0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/main.js',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','main.js'));
+});
+var names=[];
+app.get('/submit-name',function(req,res){
+    var name= req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 
-var port = 8080; 
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
