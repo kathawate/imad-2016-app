9f11b790bdbc8de01ffba74f2192b1bc378c6dc5
kathawate <srkathawate@gmail.com>
2016-11-18 19:12:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 
 
-var pool=require('pg').pool;
+var Pool = require('pg').Pool;
 var config = {
   user :'kathawate',
   database:'kathawate',
@@ -14,9 +14,11 @@ var config = {
 
 
 
+
 var app = express();
 app.use(morgan('combined'));
- var articleone={
+var articles={
+  'article-one':{
     title:' article-one |kathawate',
     heading:'article-one',
     date:'nov 6, 2016',
@@ -27,8 +29,8 @@ app.use(morgan('combined'));
         <p>
              My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
             </p>`
-};
-var articletwo={
+},
+  'article-two':{
      title:' article-two |kathawate',
     heading:'article-two',
     date:'nov 6, 2016',
@@ -40,8 +42,8 @@ var articletwo={
              My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
             </p>`
     
-};
-  var articlethree={
+},
+   'article-three':{
      title:' article-three |kathawate',
     heading:'article-three',
     date:'nov 6, 2016',
@@ -52,6 +54,8 @@ var articletwo={
         <p>
              My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming... My name is shubham kathawate.i'm 21 years old and i love the programming...
             </p>`
+}
+    
 };
 function createTemplate(data){
     var title=data.title;
@@ -80,39 +84,33 @@ var htmlTemplate=`
          ${heading}
         </h1>
     <div>
-        ${date}
+        ${date.toDateString()}
     </div>
     <div>
          ${content}
     </div>
     </div>
  </body>     
-
 </html>
-
 `;
     return htmlTemplate;
 }
-
-
-
-//app.get('/:articleName',function (req,res){
-//var articleName=req.params.articleName;
- //   res.send(createTemplate(articles[articleName]));
-//});
-
-app.get('/articleone', function (req, res) {
-   res.send(createTemplate(articleone));
+app.get('/articles/:articleName',function (req,res){
+    
+   pool.query("SELECT * FROM article WHERE title=$1" ,[req.params.articleName],function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.lenght ===0){
+               res.status(404).send('Article not found');
+           }else{
+               var articleData =result.rows[0];
+                 res.send(createTemplate(articleData));
+           }
+       }
+   });
+  
 });
-
-app.get('/articletwo', function (req, res) {
-   res.send(createTemplate(articletwo));
-});
-app.get('/articlethree', function (req, res) {
-   res.send(createTemplate(articlethree));
-});
-
-
 
 
 
@@ -129,7 +127,7 @@ app.get('/', function (req, res) {
 
 
 
-var pool = new pool(config);
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM article',function(err,result){
        if(err){
@@ -151,7 +149,6 @@ app.get('/counter',function(req,res){
     counter = counter + 1;
     res.send(counter.toString());
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
